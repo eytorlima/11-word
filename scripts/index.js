7608b48ec6f75ca0cd653f297528a46b83480f42
eytorlima <eytorlima@outlook.com>
2023-09-06 11:01:34 -0300

    adding functions

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,9 @@ const arrayTeclas = Array.from(teclas);
 const boxes = document.querySelectorAll(".box");
 const arrayBoxes = Array.from(boxes);
 //////
+const keyDelete = document.querySelector(".delete");
+const keyEnter = document.querySelector(".enter");
+//////
 var userTry = '';
 var letter = '';
 var count = 0;
@@ -18,59 +21,65 @@ for(let i = 0; i < arrayTeclas.length; i++){
     arrayTeclas[i].addEventListener("click", () => {
         letter = arrayTeclas[i].textContent;
         if(count < 5){
-            addIpunt();
+            addInput();
             userTry += letter.toLowerCase();
-        } 
-
+            console.log(userTry);
+        }
     });
 }
 
-// // function changeInputs(){
-// //     for(boxesFlag; boxesFlag < 6;){
-// //         for(let i = 0; i < 1; i+(5*boxesFlag)){
-// //             bP0 = arrayBoxes[i].textContent = box0;
-// //             bP1 = arrayBoxes[i+1].textContent = box1;
-// //             bP2 = arrayBoxes[i+2].textContent = box2;
-// //             bP3 = arrayBoxes[i+3].textContent = box3;
-// //             bP4 = arrayBoxes[i+4].textContent = box4;
-// //             break;
-// //         }
-// //         break;
-// //     }
-// // }
+keyDelete.addEventListener("click", () => {
+    letter = '';
+    if(count > 0){
+        count--;
+    }
+    userTry = userTry.substring(0, userTry.length - 1);
+    addInput();
+});
 
-// function addIpunt(){
-//     switch(count){
-//         case 0:
-//             box0 = letter;
-//             if(letter !== ''){
-//                 count++;
-//             }
-//             break;
-//         case 1:
-//             box1 = letter;
-//             if(letter !== ''){
-//                 count++;
-//             }
-//             break;
-//         case 2:
-//             box2 = letter;
-//             if(letter !== ''){
-//                 count++;
-//             }
-//             break;
-//         case 3:
-//             box3 = letter;
-//             if(letter !== ''){
-//                 count++;
-//             }
-//             break;
-//         case 4:
-//             box4 = letter;
-//             if(letter !== ''){
-//                 count++;
-//             }
-//             break;
-//     }
-//     // changeInputs();
-// }
+function addInput(){
+    switch(count){
+        case 0:
+            box0 = letter;
+            if(letter !== ''){
+                count++;
+            }
+            break;
+        case 1:
+            box1 = letter;
+            if(letter !== ''){
+                count++;
+            }
+            break;
+        case 2:
+            box2 = letter;
+            if(letter !== ''){
+                count++;
+            }
+            break;
+        case 3:
+            box3 = letter;
+            if(letter !== ''){
+                count++;
+            }
+            break;
+        case 4:
+            box4 = letter;
+            if(letter !== ''){
+                count++;
+            }
+            break;
+    }
+    changeInputs();
+}
+
+function changeInputs(){
+    for(let i = 0; i < 1; i+(5*boxesFlag)){
+        bP0 = arrayBoxes[i].textContent = box0;
+        bP1 = arrayBoxes[i+1].textContent = box1;
+        bP2 = arrayBoxes[i+2].textContent = box2;
+        bP3 = arrayBoxes[i+3].textContent = box3;
+        bP4 = arrayBoxes[i+4].textContent = box4;
+        break;
+    }
+}
